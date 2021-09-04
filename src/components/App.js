a7dbc3079691e4f25a8cb9c324d17133f467afe3
izayed9 <izayed9@gmail.com>
2021-09-05 01:13:06 +0600

    homepagelayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
+import "../styles/App.css";
+import { Layout } from "./Layout";
+import { Home } from "./pages/Home";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Layout>
+        <Home />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
